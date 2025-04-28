6ed11b41059a7119133918038f41e6f4662d1a41
Marcin Jakubowski <marcin648@gmail.com>
2025-04-28 03:19:06 +0200

    feat: types

---
+++ main/types.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <cstdint>
+#include <cstddef>
+
+// Unsigned integers
+using u8  = std::uint8_t;
+using u16 = std::uint16_t;
+using u32 = std::uint32_t;
+using u64 = std::uint64_t;
+
+// Signed integers
+using s8  = std::int8_t;
+using s16 = std::int16_t;
+using s32 = std::int32_t;
+using s64 = std::int64_t;
+
+// Floating point types
+using f32 = float;
+using f64 = double;
+
+// Size type
+using usize = std::size_t;
