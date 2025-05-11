c35e6e163b28e4ab377866e0ceb95eecc9fb4ab0
Marcin Jakubowski <marcin648@gmail.com>
2025-05-11 18:48:24 +0200

    feat: network

---
+++ main/network.cpp
@@ -0,0 +1,157 @@
+#include "network.hpp"
+
+#include <Arduino.h>
+#include <WiFi.h>
+
+Net::Network* Net::Network::_network = nullptr;
+
+void Net::Network::_on_recv(const esp_now_recv_info_t* esp_now_info, const u8* data, int size) {
+    if (size < sizeof(Packet)) {
+        return;
+    }
+
+    const Packet* packet = reinterpret_cast<const Packet*>(data);
+    if (packet->magic != MAGIC) {
+        return;
+    }
+
+    if (_network) {
+        switch (static_cast<PacketType>(packet->type)) {
+            case PacketType::HEARTBEAT:
+                _network->_on_heartbeat(esp_now_info->src_addr);
+                break;
+            case PacketType::DATA:
+                _network->_on_data(esp_now_info->src_addr, packet->message_type, packet->payload, size - sizeof(Packet));
+                break;
+        }
+    }
+}
+
+void Net::Network::_on_heartbeat(const u8* peer_mac) {
+    if (!_network) {
+        return;
+    }
+
+    _network->_last_heartbeat = millis();
+
+    if (_network->_state != NetworkState::CONNECTED) {
+       _network->_on_connected();
+    }
+
+    _network->add_peer(peer_mac);
+
+    if (_network->_heartbeat_callback) {
+        _network->_heartbeat_callback(_network->_heartbeat_callback_arg);
+    }
+}
+
+void Net::Network::_on_data(const u8* peer_mac, u16 message_type, const u8* data, usize size) {
+    if (!_network) {
+        return;
+    }
+
+    if (_network->_data_callback) {
+        _network->_data_callback(_network->_data_callback_arg, message_type, data, size);
+    }
+}
+
+void Net::Network::_on_connected() {
+    if (!_network) {
+        return;
+    }
+
+    _network->_state = NetworkState::CONNECTED;
+    if(_network->_connected_callback) {
+        _network->_connected_callback(_network->_connected_callback_arg);
+    }
+}
+
+void Net::Network::_on_disconnected() {
+    if (!_network) {
+        return;
+    }
+
+    _network->_state = NetworkState::DISCONNECTED;
+    if(_network->_disconnected_callback) {
+        _network->_disconnected_callback(_network->_disconnected_callback_arg);
+    }
+}
+
+void Net::Network::_send(PacketType type, u16 message_type, const u8* data, usize size, const u8* peer_mac) {
+    Packet packet = {0};
+    packet.magic = MAGIC;
+    packet.type = type;
+    packet.message_type = message_type;
+
+    usize payload_size = std::min(size, MAX_PAYLOAD_SIZE);
+
+    memcpy(packet.payload, data, payload_size);
+
+    esp_now_send(peer_mac, reinterpret_cast<const u8*>(&packet), sizeof(packet) + payload_size);
+}
+
+void Net::Network::begin() {
+    WiFi.mode(WIFI_STA);
+    esp_now_init();
+
+    _network = this;
+    esp_now_register_recv_cb(_on_recv);
+}
+
+void Net::Network::add_peer(const u8* peer_mac) {
+    if (esp_now_is_peer_exist(peer_mac)) {
+        return;
+    }
+
+    esp_now_peer_info_t peer_info = {0};
+    memcpy(peer_info.peer_addr, peer_mac, 6);
+    peer_info.channel = 0;
+    peer_info.ifidx = WIFI_IF_STA;
+    peer_info.encrypt = false;
+
+    esp_now_add_peer(&peer_info);
+}
+
+void Net::Network::delete_peer(const u8* peer_mac) {
+    esp_now_del_peer(peer_mac);
+}
+
+void Net::Network::send(u8 message_type, const u8* data, usize size, const u8* peer_mac) {
+    if(this->_state != NetworkState::CONNECTED) {
+        return;
+    }
+
+    this->_send(PacketType::DATA, message_type, data, size, peer_mac);
+}
+
+void Net::Network::attach_heartbeat_callback(std::function<void(void*)> callback, void* arg) {
+    this->_heartbeat_callback = callback;
+    this->_heartbeat_callback_arg = arg;
+}
+
+void Net::Network::attach_data_callback(std::function<void(void*, u16, const u8*, usize)> callback, void* arg) {
+    this->_data_callback = callback;
+    this->_data_callback_arg = arg;
+}
+
+void Net::Network::attach_connected_callback(std::function<void(void*)> callback, void* arg) {
+    this->_connected_callback = callback;
+    this->_connected_callback_arg = arg;
+}
+
+void Net::Network::attach_disconnected_callback(std::function<void(void*)> callback, void* arg) {
+    this->_disconnected_callback = callback;
+    this->_disconnected_callback_arg = arg;
+}
+
+void Net::Network::update() {
+    u32 now = millis();
+    if (now - this->_heartbeat_timer > HEARTBEAT_INTERVAL) {
+        this->_heartbeat_timer = now;
+        this->_send(PacketType::HEARTBEAT);
+    }
+
+    if (this->_state == NetworkState::CONNECTED && now - this->_last_heartbeat > HEARTBEAT_TIMEOUT) {
+        this->_on_disconnected();
+    }
+}
---
+++ main/network.hpp
@@ -0,0 +1,76 @@
+#pragma once
+
+#include <functional>
+
+#include <types.hpp>
+#include <esp_now.h>
+
+namespace Net {
+    static const u32 MAGIC = 0xC0FFEE;
+    static const usize MAX_PAYLOAD_SIZE = 250;
+
+    static const u32 HEARTBEAT_INTERVAL = 1000;
+    static const u32 HEARTBEAT_TIMEOUT = 5000;
+
+    enum class PacketType : u8 {
+        HEARTBEAT = 0,
+        DATA = 1,
+    };
+
+    struct Packet {
+        u32 magic;
+        PacketType type;
+        u8 message_type;
+        u8 payload[MAX_PAYLOAD_SIZE];
+    };
+
+    enum class NetworkState : u8 {
+        DISCONNECTED = 0,
+        CONNECTED = 1,
+    };
+
+    class Network {
+    public:
+        Network() = default;
+
+        void begin();
+
+        void add_peer(const u8* peer_mac);
+        void delete_peer(const u8* peer_mac);
+
+        void send(u8 message_type, const u8* data = nullptr, usize size = 0, const u8* peer_mac = nullptr);
+
+        void attach_heartbeat_callback(std::function<void(void*)> callback, void* arg = nullptr);
+        void attach_data_callback(std::function<void(void*, u16, const u8*, usize)> callback, void* arg = nullptr);
+        void attach_connected_callback(std::function<void(void*)> callback, void* arg = nullptr);
+        void attach_disconnected_callback(std::function<void(void*)> callback, void* arg = nullptr);
+
+        void update();
+    protected:
+        static Network* _network;
+        NetworkState _state = NetworkState::DISCONNECTED;
+        
+        u32 _last_heartbeat = 0;
+        u32 _heartbeat_timer = 0;
+
+        std::function<void(void*)> _heartbeat_callback = nullptr;
+        void* _heartbeat_callback_arg = nullptr;
+
+        std::function<void(void*, u16, const u8*, usize)> _data_callback = nullptr;
+        void* _data_callback_arg = nullptr;
+
+        std::function<void(void*)> _connected_callback = nullptr;
+        void* _connected_callback_arg = nullptr;
+
+        std::function<void(void*)> _disconnected_callback = nullptr;
+        void* _disconnected_callback_arg = nullptr;
+
+        void _send(PacketType type, u16 message_type = 0, const u8* data = nullptr, usize size = 0, const u8* peer_mac = nullptr);
+        static void _on_recv(const esp_now_recv_info_t* esp_now_info, const u8* data, int size);
+
+        static void _on_heartbeat(const u8* peer_mac);
+        static void _on_data(const u8* peer_mac, u16 message_type, const u8* data, usize size);
+        static void _on_connected();
+        static void _on_disconnected();
+    };
+}
