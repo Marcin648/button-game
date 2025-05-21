b6c140712643b0c48eeddc27f5dd0d63dd206621
Marcin Jakubowski <marcin648@gmail.com>
2025-05-22 01:20:39 +0200

    feat(master): HID

--- main/master.cpp
+++ main/master.cpp
@@ -3,6 +3,7 @@
 #include "button.hpp"
 #include "led.hpp"
 #include "network.hpp"
+#include "hid_keyboard.hpp"
 #include "game_master.hpp"
 
 Button button(CONFIG_BUTTON_GAME_PIN_BUTTON);
@@ -10,12 +11,17 @@ LED led_power(CONFIG_BUTTON_GAME_PIN_LED_POWER);
 LED led_button(CONFIG_BUTTON_GAME_PIN_LED_BUTTON);
 Net::Network network;
 Game::Master master;
+HIDKeyboard keyboard;
 
 void setup() {
     Serial.begin(115200);
     network.begin();
     master.begin(&button, &led_button, &network);
     led_power.set(LED::State::ON);
+
+    if (button.read() == LOW) {
+        keyboard.begin();
+    }
 }
 
 void loop() {
@@ -23,5 +29,6 @@ void loop() {
     led_power.update();
     led_button.update();
     button.update();
+    keyboard.update();
     delay(1);
 }
